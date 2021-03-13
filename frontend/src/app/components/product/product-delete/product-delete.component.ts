a12e41469b59339d49c13726fe953eb903d204b0
matcsaleh <matcsaleh@gmail.com>
2021-03-12 22:52:32 -0300

    first commit

---
+++ frontend/src/app/components/product/product-delete/product-delete.component.ts
@@ -0,0 +1,32 @@
+import { Product } from './../product.model';
+import { ProductService } from './../product.service';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-product-delete',
+  templateUrl: './product-delete.component.html',
+  styleUrls: ['./product-delete.component.css']
+})
+export class ProductDeleteComponent implements OnInit {
+
+  product: Product;
+  constructor( private productService : ProductService,private router : Router, private route: ActivatedRoute ) { }
+
+  ngOnInit(): void {
+    const id = this.route.snapshot.paramMap.get('id');
+    this.productService.readById(id).subscribe( product => {
+      this.product = product;
+    })
+  }
+
+  delete():void {
+      this.productService.delete(`${this.product.id}`).subscribe( () => {
+      this.productService.showMessage("Produto deletado")
+      this.router.navigate(["/products"]);
+    })
+  }
+  cancel(): void {
+    this.router.navigate(["/products"]);
+  }
+}
